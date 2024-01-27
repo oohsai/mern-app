6417b33cac8539beb6e2efe0ae845a87157788b9
oohsai <s72454726@gmail.com>
2024-01-28 03:04:29 +0530

    add basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 };
 export default App;
