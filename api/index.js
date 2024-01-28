a521ce91d1233fb5a795bf4402782dc5959f0eb0
oohsai <s72454726@gmail.com>
2024-01-29 01:24:10 +0530

    created sign in api route

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.stats(statusCode).jsona({
+  return res.status(statusCode).json({
     success: false,
     message,
     statusCode,
